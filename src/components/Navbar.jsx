23dde76e39e5b088930d03e1adf4b4b9f1a622f7
JasurbekJovliyev123 <jasurbekjovliyev725@gmail.com>
2025-06-25 17:55:22 +0500

    AS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,94 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import logo from '../assets/logo.svg';
+import { NavLink } from 'react-router-dom';
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
+import { Dialog, DialogContent, DialogTrigger, DialogClose } from '../components/ui/dialog';
+
 const Navbar = () => {
   return (
-    <header className='container py-5 flex items-center justify-between'>
-          <NavLink to={'/'}>
-            <img className='w-[60px] h-[60px]' src={logo} alt="" />
-          </NavLink>
-          <nav className='flex items-center gap-x-5'>
-            <NavLink to={'/'} className="hover:text-[#245d30] text-black font-medium transition text-[16px]">Bosh sahifa</NavLink>
-            <NavLink to={'/products'} className="hover:text-[#245d30] text-black font-medium transition text-[16px]">Mahsulotlar</NavLink>
-            <NavLink to={'/contact'} className="hover:text-[#245d30] text-black font-medium transition text-[16px]">Aloqa</NavLink>
-            <NavLink to={'/blog'} className="hover:text-[#245d30] text-black font-medium transition text-[16px]">Blog</NavLink>
-          </nav>
-        <NavLink to={'/korzinka'}>
-           <PiShoppingCartSimpleBold className='text-3xl cursor-pointer'/>
+    <header className='container py-5 px-3 flex items-center justify-between'>
+      <NavLink to={'/'}>
+        <img className='w-[60px] h-[60px]' src={logo} alt="Logo" />
+      </NavLink>
+      <nav className='md:flex hidden items-center gap-x-5'>
+        <NavLink 
+          to={'/'} 
+          className="hover:text-[#245d30] text-black font-medium transition text-[16px]"
+        >
+          Bosh sahifa
         </NavLink>
+        <NavLink 
+          to={'/products'} 
+          className="hover:text-[#245d30] text-black font-medium transition text-[16px]"
+        >
+          Mahsulotlar
+        </NavLink>
+        <NavLink 
+          to={'/contact'} 
+          className="hover:text-[#245d30] text-black font-medium transition text-[16px]"
+        >
+          Aloqa
+        </NavLink>
+        <NavLink 
+          to={'/blog'} 
+          className="hover:text-[#245d30] text-black font-medium transition text-[16px]"
+        >
+          Blog
+        </NavLink>
+      </nav>
+
+      <div className='flex items-center gap-x-4'>
+        <NavLink to={'/korzinka'}>
+          <PiShoppingCartSimpleBold className='text-3xl cursor-pointer'/>
+        </NavLink>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <button className='p-2 bg-[#245D30] md:hidden block rounded-md cursor-pointer'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="22" viewBox="0 0 27 25">
+                <path fill="#FFF" fillRule="evenodd" d="M2.5 0h22a2.5 2.5 0 1 1 0 5h-22a2.5 2.5 0 1 1 0-5zm0 10h22a2.5 2.5 0 1 1 0 5h-22a2.5 2.5 0 1 1 0-5zm0 10h22a2.5 2.5 0 1 1 0 5h-22a2.5 2.5 0 1 1 0-5z"></path>
+              </svg>
+            </button>
+          </DialogTrigger>
+
+          {/* Modal Content */}
+          <DialogContent className='absolute top-72 h-[60vh] bg-[#245D30] py-12'>
+            <nav className='flex flex-col md:hidden items-start gap-y-3'>
+              <NavLink 
+                to={'/'} 
+                className="text-white font-medium transition text-[18px]"
+                onClick={() => { document.getElementById('modal-close-btn').click(); }}
+              >
+                Bosh sahifa
+              </NavLink>
+              <NavLink 
+                to={'/products'} 
+                className="text-white font-medium transition text-[18px]"
+                onClick={() => { document.getElementById('modal-close-btn').click(); }}
+              >
+                Mahsulotlar
+              </NavLink>
+              <NavLink 
+                to={'/contact'} 
+                className="text-white font-medium transition text-[18px]"
+                onClick={() => { document.getElementById('modal-close-btn').click(); }} 
+              >
+                Aloqa
+              </NavLink>
+              <NavLink 
+                to={'/blog'} 
+                className="text-white font-medium transition text-[18px]"
+                onClick={() => { document.getElementById('modal-close-btn').click(); }}
+              >
+                Blog
+              </NavLink>
+            </nav>
+
+          </DialogContent>
+        </Dialog>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,49 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../components/ui/carousel';
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 const Products = () => {
-  // const { isPending, error, data } = useQuery({
-  //   queryKey: ['repoData'],
-  //   queryFn: () =>
-  //     fetch('https://api.piknicuz.com/api/products').then((res) =>
-  //       res.json(),
-  //     ),
-  // })
-  // console.log(data);
-  
-  const { isPending:pending1, error:error1, data:data1 } = useQuery({
+  const { isPending: pending1, error: error1, data: data1 } = useQuery({
     queryKey: ['repoData'],
     queryFn: () =>
       fetch('https://api.piknicuz.com/api/categories').then((res) =>
-        res.json(),
+        res.json()
       ),
-  })
+  });
   console.log(data1);
-  return (
-    <div>Products</div>
-  )
-}
+  
+ 
 
-export default Products
+  return (
+    <div className='container min-h-[80vh] py-16'>
+      <Carousel>
+        <div className='w-full  ml-auto flex items-center justify-end gap-x-3'>
+            <CarouselNext className='p-2 bg-[#245d30] rounded-full text-xl text-white cursor-pointer'> <MdOutlineKeyboardArrowLeft/></CarouselNext>
+            <CarouselPrevious className='p-2 bg-[#245d30] rounded-full text-xl text-white cursor-pointer'><MdOutlineKeyboardArrowRight/></CarouselPrevious>
+        </div>
+          <CarouselContent className="grid1 md:grid-cols-3 grid-cols-2 lg:grid-cols-5 gap-x-3 md:gap-x-6 w-full">
+            {data1 &&
+              data1.data.map((item) => {
+                return (
+                  <div>
+                      <CarouselItem
+                    className="flex hover:border w-[140px] md:w-[200px] rounded-full md:p-4 p-3 items-center justify-center flex-shrink-0"
+                    key={item.id}
+                  >
+                    {/* <img src={item.image_src} alt={item.name} className="w-full" /> */}
+                    <div className='text-sm md:text-[18px]'>{item.name}</div>
+                  </CarouselItem>
+                  </div>
+
+                );
+              })}
+          </CarouselContent>
+</Carousel>
+
+
+    </div>
+  );
+};
+
+export default Products;
